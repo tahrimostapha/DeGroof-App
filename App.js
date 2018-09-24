e0c2f03140f40b58c6d1224c8562e21d2cc7af6a
julie doms <juliedoms@MacBook-Pro-de-julie-2.local>
2018-09-24 15:22:07 +0200

    hihihihi

--- App.js
+++ App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import { Container, Header, Content, Button, Text } from 'native-base';
+
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
+async componentWillMount() {
+  await Expo.Font.loadAsync({
+    'Roboto': require('native-base/Fonts/Roboto.ttf'),
+    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+  });
+}
+
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -20,6 +29,9 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
+          <Button>
+            <Text>Mon premier bouton</Text>
+          </Button>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           <AppNavigator />
         </View>
