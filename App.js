7ba74375a527a646a2fbd2338aa079a343aa8151
Sammuel JANSSENS <sammueljnssns@gmail.com>
2018-09-25 12:13:10 +0200

    Ceci est un push

--- App.js
+++ App.js
@@ -9,13 +9,12 @@ export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
-<<<<<<< HEAD
-async componentWillMount() {
-  await Expo.Font.loadAsync({
-    'Roboto': require('native-base/Fonts/Roboto.ttf'),
-    'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-  });
-}
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
--- screens/ProjectPage.js
+++ screens/ProjectPage.js
@@ -13,43 +13,63 @@ export default class ListAvatarExample extends Component {
 			<List>
 				<ListItem avatar>
 				  <Left>
-				    <Thumbnail source={{ uri: 'https://pmcdeadline2.files.wordpress.com/2017/09/ap_17272739120053.jpg?w=446&h=299&crop=1' }} />
+				    <Thumbnail source={{ uri: 'https://static.thenounproject.com/png/101346-200.png' }} />
 				  </Left>
 				  <Body>
 				    <Text>Name</Text>
-				    <Text note>This is the name</Text>
+				    <Text note>JeanLuc Farming</Text>
 				  </Body>
 				</ListItem>
 				<ListItem avatar>
 				  <Left>
-				    <Thumbnail source={{ uri: 'https://pmcdeadline2.files.wordpress.com/2017/09/ap_17272739120053.jpg?w=446&h=299&crop=1' }} />
+				    <Thumbnail source={{ uri: 'https://cdn0.iconfinder.com/data/icons/seo-and-marketing/32/local-places-512.png' }} />
 				  </Left>
 				  <Body>
 				    <Text>Region</Text>
-				    <Text note>This is the region</Text>
+				    <Text note>Habergy, 6782 Hainaut</Text>
 				  </Body>
 				</ListItem>
 				<ListItem avatar>
 				  <Left>
-				    <Thumbnail source={{ uri: 'https://pmcdeadline2.files.wordpress.com/2017/09/ap_17272739120053.jpg?w=446&h=299&crop=1' }} />
+				    <Thumbnail source={{ uri: 'https://cdn3.iconfinder.com/data/icons/glypho-generic-icons/64/input-field-search-field-outline-512.png' }} />
 				  </Left>
 				  <Body>
 				    <Text>Field</Text>
-				    <Text note>Education, social</Text>
+				    <Text note>Education & Farming</Text>
 				  </Body>
 				</ListItem>
 				<ListItem avatar>
 				  <Left>
-				    <Thumbnail source={{ uri: 'https://pmcdeadline2.files.wordpress.com/2017/09/ap_17272739120053.jpg?w=446&h=299&crop=1' }} />
+				    <Thumbnail source={{ uri: 'https://cdn3.iconfinder.com/data/icons/essentials-vol-1-1/512/Type-512.png' }} />
 				  </Left>
 				  <Body>
-				    <Text>Region</Text>
-				    <Text note>This is the region</Text>
+				    <Text>Type</Text>
+				    <Text note>Coaching</Text>
+				  </Body>
+				</ListItem>
+				<ListItem avatar>
+				  <Left>
+				    <Thumbnail source={{ uri: 'https://vignette.wikia.nocookie.net/diamondhuntonline6455/images/b/b3/Skill-icon.png/revision/latest?cb=20170509083140' }} />
+				  </Left>
+				  <Body>
+				    <Text>Skill Needed</Text>
+				    <Text note>Planting, Vrooming and Tractoring</Text>
+				  </Body>
+				</ListItem>
+				<ListItem avatar>
+				  <Left>
+				    <Thumbnail source={{ uri: 'https://cdn1.iconfinder.com/data/icons/material-device/20/access-time-512.png' }} />
+				  </Left>
+				  <Body>
+				    <Text>Hours Needed</Text>
+				    <Text note>4 Hours</Text>
 				  </Body>
 				</ListItem>
 			</List>
-			<Button rounded>
-	           <Text>Primary</Text>
+			<Text style={{textTransform: 'uppercase', fontWeight : 'bold', marginTop : 35}}>Mission Description</Text>
+			<Text note>Même si on se ment, ce n'est pas un simple sport car on est tous capables de donner des informations à chacun car l'aboutissement de l'instinct, c'est l'amour ! Donc on n'est jamais seul spirituellement !</Text>
+			<Button full primary>
+	           <Text>Apply for this mission</Text>
 	        </Button>
         </Content>
       </Container>
