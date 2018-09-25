387587949ec6e9e1db48756f19675f7e77b7ea4c
julie doms <juliedoms@MacBook-Pro-de-julie-2.local>
2018-09-25 09:27:43 +0200

    juju

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Form, Item, Input } from 'native-base';
-export default class FormExample extends Component {
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+export default class AnatomyExample extends Component {
   render() {
     return (
-      <Container>
-        <Header />
+      <Container style={styles.container}>
+        <Header>
+          <Left>
+            <img src="logo.png" alt="logo degroof" height="42" width="42"> 
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
         <Content>
           <Form>
             <Item>
@@ -13,9 +21,27 @@ export default class FormExample extends Component {
             <Item last>
               <Input placeholder="Password" />
             </Item>
+         <Button success><Text> Valider </Text></Button>
           </Form>
         </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
     );
   }
 }
+ 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "black",
+  },
+});
+
+      
+      
+       
