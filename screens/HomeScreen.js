f4f0f37fbc75a3d131e60fddb5fae5a38b9a188e
julie doms <juliedoms@MacBook-Pro-de-julie-2.local>
2018-09-25 16:13:15 +0200

    kdd

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,35 +12,38 @@ export default class HomeScreen extends Component {
 
         <Container>
             <ImageBackground source={require('../cover_grey.png')} style={{width: '100%', height: '100%', resizeMode: 'cover'}}>
-        <Content>
-        
-             <Image 
-                
-                source={require('../logo_DP.png')} 
-                style={{
-                        width:120, 
-                        marginLeft: 15 ,
-                        resizeMode: 'contain',}}
-                
-            />
+                <Content>
+                    <Image 
+                        source={require('../logo_DP.png')} 
+                        style={{
+                                width:120, 
+                                marginLeft: 15 ,
+                                resizeMode: 'contain',}}    
+                     />
 
-            <Text style={{ textAlign: 'center', marginBottom: 25 , fontSize: 26 }} > My DP Projects </Text>
-            <Form >
-            <Item style={{ marginBottom: 25 }}>
-                <Input placeholder="Username" />
-                    </Item>
-            <Item last style={{ marginBottom: 30 }}>
-              <Input placeholder="Password" />
-            </Item>
+                    <Text style={{ 
+                                 textAlign: 'center', 
+                                 marginBottom: 25 , 
+                                 fontSize: 26 }} 
+                        > My DP Projects </Text>
 
-         <Button success style={{ marginLeft: 40 }}><Text> Login </Text></Button>
-          
-          <Button transparent success block style={{ marginTop: 80 }}>
-            <Text style={{textAlign: 'center'}} >Sign Up</Text></Button>
-        </Form>
-        </Content>
-        </ImageBackground>
-      </Container>
+                    <Form >
+
+                        <Item style={{ marginBottom: 25 }}>
+                            <Input placeholder="Username" />
+                        </Item>
+                        <Item last style={{ marginBottom: 30 }}>
+                            <Input placeholder="Password" />
+                        </Item>
+
+                        <Button success style={{ marginLeft: 40 }}><Text> Login </Text></Button>
+
+                        <Button transparent success block style={{ marginTop: 80 }}>
+                        <Text style={{textAlign: 'center'}} >Sign Up</Text></Button>
+                    </Form>
+                </Content>
+            </ImageBackground>
+        </Container>
     );
   }
 }
