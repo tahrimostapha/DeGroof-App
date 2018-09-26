61cbc37c4fb28e9cae83aa8f32fd98f1bb6967b6
Wyns Stéphane <stewyns.pro@gmail.com>
2018-09-26 10:57:36 +0200

    Ajout début css Settingscreen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -27,44 +27,49 @@ export default class SettingsScreen extends Component {
     
   render() {
     return (
-      <Container>
-        <Content>
+      <Container style={styles.container}>
           <Image
             style={{height: 250, flex:1}}
             source={{uri: 'https://e3.365dm.com/18/04/1096x616/skynews-kim-jong-un-north-korea_4278358.jpg'}}
           />
-          <Text>Personnal Info Page</Text>
-          <Text>Username : {this.state.username}</Text>
-          <Text>Adress : {this.state.adress}</Text>
-          <Text>Region : {this.state.region}</Text>
-          <Text>SKILL (Selected in the list) {this.state.skill}</Text>
-          <Form>
-            <Picker
-              mode="dropdown"
-              iosHeader="Select your SIM"
-              iosIcon={<Icon name="arrow-dropdown-circle" style={{ color: "#007aff", fontSize: 25 }} />}
-              style={{ width: undefined }}
-              selectedValue={this.state.selected}
-              onValueChange={this.onValueChange.bind(this)}
-            >
-              <Picker.Item label="Management" value="key0" />
-              <Picker.Item label="Financial Consulting" value="key1" />
-              <Picker.Item label="Market Survey" value="key2" />
-              <Picker.Item label="RGPD Consulting" value="key3" />
-              <Picker.Item label="Tax Accountant " value="key4" />
-            </Picker>
-          </Form>
-          <Text>Total Hour on app : {this.state.hourCount}</Text>
-        </Content>
+          <Content style={styles.content}>
+            <Text>Personnal Info Page</Text>
+            <Text>Username : {this.state.username}</Text>
+            <Text>Adress : {this.state.adress}</Text>
+            <Text>Region : {this.state.region}</Text>
+            <Text>SKILL (Selected in the list) {this.state.skill}</Text>
+            <Form>
+              <Picker
+                mode="dropdown"
+                iosHeader="Select your SIM"
+                iosIcon={<Icon name="arrow-dropdown-circle" style={{ color: "#007aff", fontSize: 25 }} />}
+                style={{ width: undefined }}
+                selectedValue={this.state.selected}
+                onValueChange={this.onValueChange.bind(this)}
+              >
+                <Picker.Item label="Management" value="key0" />
+                <Picker.Item label="Financial Consulting" value="key1" />
+                <Picker.Item label="Market Survey" value="key2" />
+                <Picker.Item label="RGPD Consulting" value="key3" />
+                <Picker.Item label="Tax Accountant " value="key4" />
+              </Picker>
+            </Form>
+            <Text>Total Hour on app : {this.state.hourCount}</Text>
+          </Content>
       </Container>
     );
   }
 }
 
-  // const styles=StyleSheet.create({
-  //   container: {
-
-  //   },
-  // })
+  const styles=StyleSheet.create({
+    container: {
+      paddingLeft: 5,
+      paddingRight: 15,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    content: {
+      paddingLeft: 15,
+    },
+  })
  
-       
